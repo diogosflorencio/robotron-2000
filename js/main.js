789822e73659e20d310b4a89a6e4173cb8a13dd7
Diogo S. Florêncio <diogosflorencio@gmail.com>
2023-12-22 13:37:48 -0300

    add: possiblidade de alterar os valores

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+
+const controle = document.querySelectorAll(".controle-ajuste");
+
+controle.forEach((elemento) =>{
+    elemento.addEventListener("click", (evento) =>{
+        manupulaDados(evento.target.textContent, evento.target.parentNode);
+    })
+});
+
+function manupulaDados(operacao, controle){
+    const peca = controle.querySelector(".controle-contador");
+
+    if(operacao === "-"){
+        peca.value = parseInt(peca.value) - 1;
+    }else{
+        peca.value = parseInt(peca.value) + 1;
+    }
+}
